548e1a8ef2211d77595a266d19951632a50f694c
Srinithi Mitra <22053291@kiit.ac.in>
2025-06-29 19:52:47 +0530

    Completed All Week2 Excercises

---
+++ Week2_AdvancedSQL_NUnit/Code/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("srinithimitra@gmail.com", "Hello from Srinithi");
+        }
+    }
+}
+

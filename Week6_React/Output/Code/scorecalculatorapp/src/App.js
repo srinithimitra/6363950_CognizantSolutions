3e5e7a2a506847cccffffff72d1ffbe4797907a2
Srinithi Mitra <22053291@kiit.ac.in>
2025-07-27 19:50:42 +0530

    Completed first half of react hands on

---
+++ Week6_React/Output/Code/scorecalculatorapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <CalculateScore/>
+        </div>
+  );
+}
+
+export default App;
